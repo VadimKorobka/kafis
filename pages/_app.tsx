a0946e0e05e9e1f79ec595a25c8fac5bf38c4587
Vadym Korobka <korobka.2001.2016@gmail.com>
2022-05-03 02:32:29 +0300

    Improve css optimisation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,19 @@
+import Head from "next/head";
 import "../styles/style.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <title>Кафедра інформаційних систем ХНЕУ ім. Семена Кузнеця</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import React from "react";
 import Header from "../components/header";
 
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>
+          Головна | Кафедра інформаційних систем ХНЕУ ім. Семена Кузнеця
+        </title>
+      </Head>
       <Header />
     </>
   );
